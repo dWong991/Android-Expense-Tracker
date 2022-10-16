54ef221603cf242dfc2ee0b49c8fb96fd7e16b05
D Wong <wong.david991@gmail.com>
2022-10-15 19:39:13 -0700

    cleaned up extra imports, and added comments

--- app/src/main/java/com/example/expensetracker2/MyValueFormatter.java
+++ app/src/main/java/com/example/expensetracker2/MyValueFormatter.java
@@ -1,19 +1,18 @@
 package com.example.expensetracker2;
 
 import com.github.mikephil.charting.formatter.ValueFormatter;
-
-import java.text.DecimalFormat;
 import java.util.Locale;
 
 public class MyValueFormatter extends ValueFormatter {
     private final double total;
 
     public MyValueFormatter(double Total) {
-        total = Total; // use one decimal
+        total = Total;
     }
 
     @Override
     public String getFormattedValue(float value) {
-        return "$" + String.format(Locale.US, "%.2f", (value * total)/100); // e.g. append percentage sign
+        //total * percentage to get category total
+        return "$" + String.format(Locale.US, "%.2f", (value * total)/100);
     }
 }
